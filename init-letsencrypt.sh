ced5bca4159910f115f5347fa3d50af0e068b2ed
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-10-04 12:35:59 -0300

    updating domain.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(twogatherbackend.com) #fixme se for usar o letsencrypt, tem que mudar o domain
+#domains=(api.rpgsands.com) #fixme se for usar o letsencrypt, tem que mudar o domain
+domains=(2gather.shinji.dev)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="lomeuelpidio@gmail.com" # Adding a valid address is strongly recommended
+email="shinji.matsumoto.sh@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then

e6f9c3d5ad2f123d2453dfa7bbeac5bcce22801b
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-10-11 14:42:30 -0300

    Update settings.py

--- TwoGatherBackend/settings.py
+++ TwoGatherBackend/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*'] #fixme se der problema de cors fala comigo
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_HEADERS = [
     "accept",
     "accept-encoding",

0848f77a3eaa9baeb01bcbfd1cb7e669e7d3f567
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-10-17 20:07:18 -0300

    Update settings.py

--- TwoGatherBackend/settings.py
+++ TwoGatherBackend/settings.py
@@ -117,7 +117,8 @@ if os.getenv('PRODUCTION', '') == 'True': #fixme tem que configurar a DB aqui (o
             'PORT': os.getenv('DB_PORT', '')
         }
     }
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 else:
     DATABASES = {
@@ -162,7 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

9edd106acb69b11074df6945186e68a772d7263b
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-10-29 04:02:35 -0300

    testing on deploy because why not?

--- api/modules/message/serializers.py
+++ api/modules/message/serializers.py
@@ -1,7 +1,9 @@
 from group.models import Message
+from user.serializers import PublicUserSerializer
 from rest_framework import serializers
 
 class MessageSerializer(serializers.ModelSerializer):
+    user_name = PublicUserSerializer(source='user', read_only=True, fields=('name'))
     class Meta:
         model = Message
         fields = ('__all__')

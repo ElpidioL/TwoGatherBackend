a0e316ca59054cf4cb5c0237d51770d989f0dc82
Elpidio / Nick: (TheBlackCake) <lomeuelpidio@gmail.com>
2023-11-01 21:34:00 -0300

    participants update

--- api/modules/group/serializers.py
+++ api/modules/group/serializers.py
@@ -4,7 +4,8 @@ from api.modules.user.serializers import PublicUserSerializer
 from rest_framework import serializers
 
 class GroupSerializer(serializers.ModelSerializer):
-    participants =  PublicUserSerializer(many=True)
+    participants = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True, write_only=True)
+    members = PublicUserSerializer(many=True, source="participants", read_only=True)
     class Meta:
         model = Group
         fields = ('__all__')
